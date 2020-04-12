47dddd2cbf0dd35ba78a7bc789f0dcb8152e04b2
limonadev <limonadev@whiteleaf.dev>
2020-04-12 13:01:17 -0500

    cleaner version of algorithm

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -1,80 +1,100 @@
 class PhoneNumber {
-  
-  // Put your code here
+
+  ExceptionHandler exception = ExceptionHandler();
+
   String clean(String number){
-    String cleanNumber = '';
+    RegExp skipableChars = RegExp('[ -.()+]');
+    String leanNumber = number.replaceAll(skipableChars, '');
 
-    List<String> allowedToSkip = [' ', '-', '.', '(', ')', '+'];
-    for(int i=0; i<number.length; ++i){
-      String c = number[i];
+    _containsOnlyDigits(leanNumber);
+    _hasCorrectSize(leanNumber);
 
-      if(allowedToSkip.contains(c)) continue;
-      if(!(c.codeUnitAt(0)>47 && c.codeUnitAt(0)<58)) {
-        throw(_getExceptionForError(ErrorType.NotADigit, number: c));
-      }
-      cleanNumber += c;
-    }
-    
-    dynamic validNumber = _getValidNumber(cleanNumber);
+    leanNumber =  _removeCountryCode(leanNumber);
 
-    if(validNumber is ErrorType) throw(_getExceptionForError(validNumber));
-    else return validNumber.toString();
+    _hasCorrectAreaCode(leanNumber);
+    _hasCorrectExchangeCode(leanNumber);
+
+    return leanNumber;
   }
 
-  dynamic _getValidNumber(String cleanNumber){
-    if(cleanNumber.length > 11) return ErrorType.TooManyDigits;
-    if(cleanNumber.length < 10) return ErrorType.NotEnoughDigits;
+  void _containsOnlyDigits(String number){
+    RegExp letterChars = RegExp('[a-zA-Z]');
+    if(number.contains(letterChars)) throw(exception.of(ErrorType.ContainsLetters));
 
-    if(cleanNumber.length == 11){
-      if(cleanNumber[0] != '1') return ErrorType.IntCountryCodeIsNotOne;
-      cleanNumber = cleanNumber.substring(1);
-    } 
+    RegExp punctuationChars = RegExp('[@:!]');
+    if(number.contains(punctuationChars)) throw(exception.of(ErrorType.ContainsPunctuation));
 
-    if(cleanNumber[0] == '0') return ErrorType.AreaCodeIsZero;
-    if(cleanNumber[0] == '1') return ErrorType.AreCodeIsOne;
-
-    if(cleanNumber[3] == '0') return ErrorType.ExchangeCodeIsZero;
-    if(cleanNumber[3] == '1') return ErrorType.ExchangeCodeIsOne;
-    
-    return cleanNumber;
+    RegExp nonDigitChars = RegExp('\D');
+    if(number.contains(nonDigitChars)) throw(exception.of(ErrorType.NonOnlyDigits));
   }
 
-  FormatException _getExceptionForError(ErrorType error, {String number:''}){
-    switch (error) {
-      case ErrorType.NotADigit:
-        if(number.contains(new RegExp('[@|:|!]+'))) return FormatException('punctuations not permitted');
-        if(number.contains(new RegExp('[a-zA-Z]+'))) return FormatException('letters not permitted');
-        break;
-      case ErrorType.IntCountryCodeIsNotOne:
-        return FormatException('11 digits must start with 1');
-      case ErrorType.NotEnoughDigits:
-        return FormatException('incorrect number of digits');
-      case ErrorType.TooManyDigits:
-        return FormatException('more than 11 digits');
-      case ErrorType.AreaCodeIsZero:
-        return FormatException('area code cannot start with zero');
-      case ErrorType.AreCodeIsOne:
-        return FormatException('area code cannot start with one');
-      case ErrorType.ExchangeCodeIsZero:
-        return FormatException('exchange code cannot start with zero');
-      case ErrorType.ExchangeCodeIsOne:
-        return FormatException('exchange code cannot start with one');
-      default:
-        break;
-    }
+  void _hasCorrectSize(String number){
+    if(number.length > 11) throw(exception.of(ErrorType.TooManyDigits));
+    if(number.length < 10) throw(exception.of(ErrorType.NotEnoughDigits));
+  }
 
-    return FormatException('Unhandled error type when invalid number');
+  String _removeCountryCode(String number){
+    if(number.length == 11){
+      if(!number.startsWith('1')) throw(exception.of(ErrorType.IntCountryCodeIsNotOne));
+      number = number.substring(1);
+    }
+    return number;
+  }
+
+  void _hasCorrectAreaCode(String number){
+    if(number.length == 11) number = number.substring(1);
+
+    if(number.startsWith('0')) throw(exception.of(ErrorType.AreaCodeIsZero));
+    if(number.startsWith('1')) throw(exception.of(ErrorType.AreaCodeIsOne));
+  }
+
+  void _hasCorrectExchangeCode(String number){
+    if(number.length == 11) number = number.substring(1);
+
+    if(number[3] == '0') throw(exception.of(ErrorType.ExchangeCodeIsZero));
+    if(number[3] == '1') throw(exception.of(ErrorType.ExchangeCodeIsOne));
   }
 
 }
 
+
+class ExceptionHandler {
+
+  final Map<ErrorType, Exception> _exceptions = {
+    ErrorType.ContainsLetters : FormatException('letters not permitted'),
+    ErrorType.ContainsPunctuation : FormatException('punctuations not permitted'),
+    ErrorType.NonOnlyDigits : FormatException('non digits chars used in string'),
+    ErrorType.IntCountryCodeIsNotOne : FormatException('11 digits must start with 1'),
+    ErrorType.NotEnoughDigits : FormatException('incorrect number of digits'),
+    ErrorType.TooManyDigits : FormatException('more than 11 digits'),
+    ErrorType.AreaCodeIsZero : FormatException('area code cannot start with zero'),
+    ErrorType.AreaCodeIsOne : FormatException('area code cannot start with one'),
+    ErrorType.ExchangeCodeIsZero : FormatException('exchange code cannot start with zero'),
+    ErrorType.ExchangeCodeIsOne : FormatException('exchange code cannot start with one'),
+    ErrorType.UnhandledError : FormatException('unhandled error type when invalid number')
+  };
+
+  Exception of(ErrorType error){
+    Exception exception;
+
+    if(_exceptions.containsKey(error)) exception = _exceptions[error];
+    else exception = _exceptions[ErrorType.UnhandledError];
+
+    return exception;
+  }
+ 
+}
+
 enum ErrorType{
-  NotADigit,
+  ContainsLetters,
+  ContainsPunctuation,
+  NonOnlyDigits,
   IntCountryCodeIsNotOne,
   NotEnoughDigits,
   TooManyDigits,
   AreaCodeIsZero,
-  AreCodeIsOne,
+  AreaCodeIsOne,
   ExchangeCodeIsZero,
-  ExchangeCodeIsOne
+  ExchangeCodeIsOne,
+  UnhandledError
 }
